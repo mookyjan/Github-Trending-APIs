8013f8b1e9d2f8bba8780e8bcd5e1b9c9fd4334b
mudassir <testingmyeg@gmail.com>
2020-03-11 15:12:55 +0800

    refactor: refactor iPreference to make it compitable with SOLID principle

---
+++ data/src/main/java/com/mudassirkhan/data/util/AppConstanst.kt
@@ -0,0 +1,5 @@
+package com.mudassirkhan.data.util
+
+object AppConstanst {
+    const val SAVED_AT_TIME = "saved_at_time"
+}
--- data/src/main/java/com/mudassirkhan/data/util/IPreference.kt
+++ data/src/main/java/com/mudassirkhan/data/util/IPreference.kt
@@ -6,34 +6,18 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class IPreference @Inject constructor(private val context: Context) {
-
-    companion object {
-        const val SAVED_AT_TIME = "saved_at_time"
-    }
-
-    private lateinit var prefs: SharedPreferences
+    private var prefs: SharedPreferences = context.getSharedPreferences("RepositoriesPref", Context.MODE_PRIVATE)
     var edit: SharedPreferences.Editor? = null
+
     init {
-        this.prefs = context.getSharedPreferences("RepositoriesPref", Context.MODE_PRIVATE)
-        edit =prefs!!.edit()
+        edit = prefs!!.edit()
     }
 
-
-    fun setSavedTime(time: Long) {
-        prefs.edit().putLong(SAVED_AT_TIME, time).apply()
+    fun setLong(key: String, time: Long) {
+        prefs.edit().putLong(key, time).apply()
     }
 
-    fun getLastTimeSaved(): Long? {
-        return prefs.getLong(SAVED_AT_TIME, 0)
-    }
-
-    /**
-     * fetch data after two hours
-     */
-    fun isFetchNeeded():Boolean{
-        val currentTime =System.currentTimeMillis()
-        val lastUpdateTime = getLastTimeSaved()
-        val hours = TimeUnit.MILLISECONDS.toHours( currentTime- lastUpdateTime!!)
-        return hours>2
+    fun getLong(key: String): Long {
+        return prefs.getLong(key, 0)
     }
 }
